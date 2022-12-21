c20ceccabea707efc184127eede659b084016ae3
Arturo Mejia <armejia02@gmail.com>
2022-12-20 20:16:59 -0600

    mostrando los puntos del jugador en el html

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,7 +8,14 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S']
 const especiales = ['A', 'J', 'Q', 'K']
+let puntosJugador = 0,
+  puntosComputadora = 0;
+// Referencias del html
+const btnPedir = document.querySelector("#btnPedir")
+const puntosHTML = document.querySelectorAll("small")
 
+
+// Esta función crea una nueva baraja
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (const tipo of tipos) {
@@ -22,10 +29,38 @@ const crearDeck = () => {
     }
   }
 
-
-  console.log(deck);
-  deck= _.shuffle(deck)
+  // console.log(deck);
+  deck = _.shuffle(deck);
   console.log(deck);
 
 }
+
 crearDeck();
+
+// Esta función me permite tomar una carta
+const perdirCarta = () => {
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+  const carta = deck.pop();
+  return carta;
+}
+
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return (isNaN(valor)) ?
+    (valor === 'A') ? 11 : 10
+    : valor * 1;
+}
+const valor = valorCarta(perdirCarta());
+
+// Eventos
+
+btnPedir.addEventListener('click', () => {
+  const carta = perdirCarta();
+  puntosJugador = puntosJugador + valorCarta(carta);
+  console.log(puntosJugador);
+  puntosHTML[0].innerText = puntosJugador
+
+});
